bcb19c20d8362d8059b401792ac2227eb95b574a
xsnok <chocowaco152@gmail.com>
2025-11-16 22:51:52 -0800

    Add loadModels function

--- clash-face-recognition/src/components/WebCam.jsx
+++ clash-face-recognition/src/components/WebCam.jsx
@@ -19,6 +19,16 @@ export default function WebCam() {
       }
 		}
 
+		async function loadModels() {
+			Promise.all([
+      	faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+				faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+				faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+				faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+			])
+		}
+
+		loadModels()
 		startWebcam()
 	})
 
